366b2087707741c85576e1b631165919938e1cc4
Rayen Heni <rayen.heni@insat.ucar.tn>
2025-11-30 12:55:01 +0100

    TP IaC

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "db_name"     { default = "devops_db" }
+variable "db_user"     { default = "devops_user" }
+variable "db_password" { default = "strongpassword123" }
+variable "app_port_external" { default = 8080 }

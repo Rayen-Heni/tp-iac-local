a02b887a35aeb5ce18490be8f0c50060b0c1ad2c
Rayen Heni <rayen.heni@insat.ucar.tn>
2025-11-30 13:05:48 +0100

    changed pass to test

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "db_name"     { default = "devops_db" }
 variable "db_user"     { default = "devops_user" }
-variable "db_password" { default = "strongpassword123" }
+variable "db_password" { default = "changeofpass" }
 variable "app_port_external" { default = 8080 }
